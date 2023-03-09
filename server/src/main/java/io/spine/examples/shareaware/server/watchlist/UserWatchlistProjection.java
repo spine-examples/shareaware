e67326fc90f8576ba645270a757e93cc0726d723
Artem Semenov <artem.semenov@teamdev.com>
2023-03-09 10:57:22 +0200

    Implement projection for watchlists.

---
+++ server/src/main/java/io/spine/examples/shareaware/server/watchlist/UserWatchlistProjection.java
@@ -0,0 +1,27 @@
+package io.spine.examples.shareaware.server.watchlist;
+
+import io.spine.core.Subscribe;
+import io.spine.examples.shareaware.WatchlistId;
+import io.spine.examples.shareaware.watchlist.UserWatchlists;
+import io.spine.examples.shareaware.watchlist.event.WatchlistCreated;
+import io.spine.server.projection.Projection;
+
+import static io.spine.examples.shareaware.watchlist.UserWatchlists.*;
+
+/**
+ * Builds display information for all user's watchlists.
+ */
+public final class UserWatchlistProjection
+        extends Projection<WatchlistId, UserWatchlists, UserWatchlists.Builder> {
+
+    @Subscribe
+    void on(WatchlistCreated e) {
+
+        WatchlistView watchlistView = WatchlistView.newBuilder()
+                                                   .setId(e.getWatchlist())
+                                                   .setName(e.getName())
+                                                   .vBuild();
+
+        builder().addWatchlist(watchlistView);
+    }
+}
