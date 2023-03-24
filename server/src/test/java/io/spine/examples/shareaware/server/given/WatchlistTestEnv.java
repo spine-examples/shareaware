cfe765cbda47b40f3940e8f7e172ad61d7dd6711
Artem Semenov <artem.semenov@teamdev.com>
2023-03-24 16:01:58 +0200

    Add tests for watching share flow.

---
+++ server/src/test/java/io/spine/examples/shareaware/server/given/WatchlistTestEnv.java
@@ -0,0 +1,85 @@
+/*
+ * Copyright 2023, TeamDev. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Redistribution and use in source and/or binary forms, with or without
+ * modification, must retain the above copyright notice and the following
+ * disclaimer.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ * OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package io.spine.examples.shareaware.server.given;
+
+import io.spine.core.UserId;
+import io.spine.examples.shareaware.Share;
+import io.spine.examples.shareaware.WatchlistId;
+import io.spine.examples.shareaware.watchlist.Watchlist;
+import io.spine.examples.shareaware.watchlist.command.CreateWatchlist;
+import io.spine.examples.shareaware.watchlist.command.WatchShare;
+import io.spine.testing.core.given.GivenUserId;
+import io.spine.testing.server.blackbox.BlackBoxContext;
+
+import static io.spine.examples.shareaware.server.given.GivenShare.*;
+import static io.spine.testing.TestValues.randomString;
+
+public final class WatchlistTestEnv {
+
+    /**
+     * Prevents instantiation of this class.
+     */
+    private WatchlistTestEnv() {
+    }
+
+    public static CreateWatchlist createWatchlist() {
+        return CreateWatchlist
+                .newBuilder()
+                .setUser(GivenUserId.generated())
+                .setWatchlist(WatchlistId.generate())
+                .setName(randomString())
+                .vBuild();
+    }
+
+    public static Watchlist setUpWatchlist(BlackBoxContext context) {
+        CreateWatchlist command = createWatchlist();
+        context.receivesCommand(command);
+        return Watchlist
+                .newBuilder()
+                .setId(command.getWatchlist())
+                .setOwner(command.getUser())
+                .setName(command.getName())
+                .vBuild();
+    }
+
+    private static WatchShare watchShare(WatchlistId watchlist, UserId owner, Share share) {
+        return WatchShare
+                .newBuilder()
+                .setWatchlist(watchlist)
+                .setUser(owner)
+                .setShare(share)
+                .vBuild();
+    }
+
+    public static WatchShare watchTeslaShare(WatchlistId watchlist, UserId owner) {
+        return watchShare(watchlist, owner, tesla());
+    }
+
+    public static WatchShare watchAppleShare(WatchlistId watchlist, UserId owner) {
+        return watchShare(watchlist, owner, apple());
+    }
+}
