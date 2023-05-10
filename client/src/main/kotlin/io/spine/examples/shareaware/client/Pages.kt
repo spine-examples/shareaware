fa66fef716e4c483c67d32fee097d6a5afb8a385
Artem Semenov <artem.semenov@teamdev.com>
2023-05-10 13:08:11 +0300

    Implement navigation menu.

--- client/src/main/kotlin/io/spine/examples/shareaware/client/Pages.kt
+++ client/src/main/kotlin/io/spine/examples/shareaware/client/Pages.kt
@@ -24,26 +24,11 @@
  * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-import androidx.compose.desktop.ui.tooling.preview.Preview
-import androidx.compose.material.Button
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
+package io.spine.examples.shareaware.client
 
-@Composable
-@Preview
-fun App() {
-    var text by remember { mutableStateOf("Hello, World!") }
-
-    MaterialTheme {
-        Button(onClick = {
-            text = "Hello, Desktop!"
-        }) {
-            Text(text)
-        }
-    }
+/**
+ * Provides the pages of the application.
+ */
+enum class Pages {
+    HOME, WALLET, MARKET, INVESTMENTS, WATCHLISTS
 }
