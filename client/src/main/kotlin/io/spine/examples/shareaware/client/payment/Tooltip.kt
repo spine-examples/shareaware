f7e37006f1ee0ffca920ae9b46ab2eea69ba4ef1
Artem Semenov <artem.semenov@teamdev.com>
2023-06-19 14:12:45 +0300

    Rewrite layout for the `Tooltip` component.

--- client/src/main/kotlin/io/spine/examples/shareaware/client/payment/Tooltip.kt
+++ client/src/main/kotlin/io/spine/examples/shareaware/client/payment/Tooltip.kt
@@ -28,11 +28,11 @@ package io.spine.examples.shareaware.client.payment
 
 import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.TooltipArea
-import androidx.compose.foundation.border
+import androidx.compose.foundation.TooltipPlacement
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.widthIn
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.outlined.Warning
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Icon
@@ -40,44 +40,65 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.DpOffset
 import androidx.compose.ui.unit.dp
+import io.spine.examples.shareaware.client.Icons
 
 /**
  * Warning tooltip component with icon.
  *
  * @param tip message shown on the icon hover
+ * @param modifier the `Modifier` to be applied to this tooltip
+ * @param iconColor the color of the icon,
+ * @param iconModifier the `Modifier` to be applied to the icon
+ * @param offset `DpOffset` to be added to the position of the popup
  */
 @OptIn(ExperimentalFoundationApi::class)
 @Composable
-public fun WarningTooltip(tip: String) {
+public fun Tooltip(
+    tip: String,
+    modifier: Modifier = Modifier,
+    iconColor: Color = MaterialTheme.colorScheme.error,
+    iconModifier: Modifier = Modifier
+        .size(13.dp),
+    offset: DpOffset = DpOffset(0.dp, 0.dp)
+) {
     TooltipArea(
         tooltip = {
-            Card (
-                shape = MaterialTheme.shapes.small,
+            Card(
+                shape = RoundedCornerShape(
+                    topStart = 0.dp,
+                    topEnd = 8.dp,
+                    bottomEnd = 8.dp,
+                    bottomStart = 8.dp
+                ),
                 colors = CardDefaults.outlinedCardColors(
-                    containerColor = MaterialTheme.colorScheme.surface
+                    containerColor = MaterialTheme.colorScheme.secondary
                 ),
                 modifier = Modifier
                     .padding(10.dp)
                     .widthIn(min = 90.dp, max = 210.dp)
-                    .border(
-                        width = 1.dp,
-                        color = MaterialTheme.colorScheme.error,
-                        shape = MaterialTheme.shapes.small
-                    )
             ) {
                 Text(
                     tip,
-                    style = MaterialTheme.typography.bodyMedium,
+                    style = MaterialTheme.typography.bodySmall,
                     modifier = Modifier.padding(10.dp)
                 )
             }
         },
         content = {
             Icon(
-                imageVector = Icons.Outlined.Warning,
-                contentDescription = "Warning",
-                tint = MaterialTheme.colorScheme.error)
-        }
+                painter = painterResource(Icons.INFO),
+                contentDescription = "Info",
+                tint = iconColor,
+                modifier = iconModifier
+            )
+        },
+        modifier = modifier,
+        tooltipPlacement = TooltipPlacement.ComponentRect(
+            offset = offset
+        )
     )
 }
